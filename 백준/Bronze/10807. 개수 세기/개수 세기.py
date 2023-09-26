038a40f00a39ad4c35698bfeddc9166823c29167
kuku627 <146042725+kuku627@users.noreply.github.com>
2023-09-26 22:07:15 +0900

    [Bronze V] Title: 개수 세기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -0,0 +1,12 @@
+# 리스트 요소 출력하는법
+# N=int(input())
+
+# arr=list(map(int,input().split()))
+# print(arr)
+
+N=int(input())
+
+arr=list(map(int,input().split()))
+v=int(input())
+
+print(arr.count(v))
