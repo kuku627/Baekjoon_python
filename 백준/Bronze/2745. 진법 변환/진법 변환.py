556a45a6737b376afc5ba42c46f591a7361f3b83
kuku627 <146042725+kuku627@users.noreply.github.com>
2023-10-06 16:17:17 +0900

    [Bronze II] Title: 진법 변환, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -0,0 +1,12 @@
+# alpha={}
+
+# for i in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+#     alpha[i]=ord(i)-ord('A')+10
+#     print(alpha[i])
+
+N,B=input().split()
+B=int(B)
+# N = [i for i in N]
+Number=int(N,B)
+# result = str(Number)
+print(Number)
