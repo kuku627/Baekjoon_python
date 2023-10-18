1b8ce2bb84c1b3104786558d666c8ed763452343
kuku627 <146042725+kuku627@users.noreply.github.com>
2023-10-18 10:39:32 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.py
@@ -0,0 +1,14 @@
+# MenOfPassion(A[], n) {
+#     sum <- 0;
+#     for i <- 1 to n
+#         for j <- 1 to n
+#             for k <- 1 to n
+#                 sum <- sum + A[i] × A[j] × A[k]; # 코드1
+#     return sum;
+# }
+
+
+n=int(input())
+
+print(n*n*n)
+print(3)
